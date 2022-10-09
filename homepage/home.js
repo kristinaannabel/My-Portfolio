96a5b9e1eb42108ad2576aabec926375545bc93c
kristinaannabel <100444032+kristinaannabel@users.noreply.github.com>
2022-10-09 23:19:38 +0200

    Added html, css and js to front page

---
+++ homepage/home.js
@@ -0,0 +1,17 @@
+function typingEffect(element, speed) {
+  let text = element.innerHTML;
+  element.innerHTML = "";
+  var i = 0;
+  var timer = setInterval(function () {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
+
+const h1 = document.querySelector("#frontdev");
+typingEffect(h1, 150);
+
